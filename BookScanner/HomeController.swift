c344b0583ce5eecf08104fa3508bea520469ab8d
David Riegel <40246197+bumothekid@users.noreply.github.com>
2023-05-07 21:57:45 +0200

    Prepare project without Storyboard

--- BookScanner/HomeController.swift
+++ BookScanner/HomeController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeController.swift
 //  BookScanner
 //
 //  Created by David Riegel on 07.05.23.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
