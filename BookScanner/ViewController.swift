3f2f832a6958330e354b57ac10f37c5792ff2240
David Riegel <40246197+bumothekid@users.noreply.github.com>
2023-05-07 21:50:47 +0200

    Initial Commit

---
+++ BookScanner/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BookScanner
+//
+//  Created by David Riegel on 07.05.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
