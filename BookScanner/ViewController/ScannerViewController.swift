037a97f01063565544e005840a1b8dc4a0601abc
David Riegel <40246197+bumothekid@users.noreply.github.com>
2023-05-07 22:47:30 +0200

    Create ScannerViewController

---
+++ BookScanner/ViewController/ScannerViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ScannerViewController.swift
+//  BookScanner
+//
+//  Created by David Riegel on 07.05.23.
+//
+
+import UIKit
+
+class ScannerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
