fb896a857c587bb7e8868aa92b336af32bc10296
David Riegel <40246197+bumothekid@users.noreply.github.com>
2023-05-08 00:04:50 +0200

    Create barcode scanner and print out data

--- BookScanner/ViewController/ScannerViewController.swift
+++ BookScanner/ViewController/ScannerViewController.swift
@@ -6,12 +6,121 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ScannerViewController: UIViewController {
+class ScannerViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
+    
+    var captureSession: AVCaptureSession!
+    var previewLayer: AVCaptureVideoPreviewLayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        Task {
+            guard await askForCameraPermissions() else { return }
+            configureViewComponents()
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        if captureSession?.isRunning == false {
+            captureSession.startRunning()
+        }
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        if captureSession?.isRunning == true {
+            captureSession.stopRunning()
+        }
+    }
+    
+    func readBarcode(_ data: String) {
+        print(data)
+    }
+    
+    func askForCameraPermissions() async -> Bool {
+        var cameraPermissions = AVCaptureDevice.authorizationStatus(for: .video)
+        
+        if cameraPermissions == AVAuthorizationStatus.notDetermined {
+            AVCaptureDevice.requestAccess(for: .video) { granted in
+                if !granted {
+                    print("Camera video permissions declined.")
+                }
+            }
+            
+            cameraPermissions = AVCaptureDevice.authorizationStatus(for: .video)
+        }
+        
+        return cameraPermissions == AVAuthorizationStatus.authorized
+    }
+    
+    func notSupported() {
+        let alertController = UIAlertController(title: "", message: "Scanning is not supported on your device.", preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "Ok", style: .default))
+        
+        present(alertController, animated: true)
+        captureSession = nil
+    }
+    
+    func configureViewComponents() {
+        view.backgroundColor = .systemBackground
+        title = "Scanner"
+        
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationItem.largeTitleDisplayMode = .always
+        
+        captureSession = AVCaptureSession()
+        guard let videoCaptureDevice = AVCaptureDevice.default(for: .video) else { return }
+        let videoInput: AVCaptureDeviceInput
+        
+        do {
+            videoInput = try AVCaptureDeviceInput(device: videoCaptureDevice)
+        }
+        catch {
+            return
+        }
+        
+        if captureSession.canAddInput(videoInput) {
+            captureSession.addInput(videoInput)
+        }
+        else {
+            notSupported()
+            return
+        }
+        
+        let metaDataOutput = AVCaptureMetadataOutput()
+        
+        if captureSession.canAddOutput(metaDataOutput) {
+            captureSession.addOutput(metaDataOutput)
+            
+            metaDataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+            metaDataOutput.metadataObjectTypes = [.ean8, .ean13, .pdf417]
+        }
+        else {
+            notSupported()
+            return
+        }
+        
+        previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        previewLayer.frame = view.layer.bounds
+        previewLayer.videoGravity = .resizeAspectFill
+        view.layer.addSublayer(previewLayer)
+        
+        captureSession.startRunning()
+    }
+    
+    func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
+        captureSession.stopRunning()
+        
+        if let metaDataObject = metadataObjects.first {
+            guard let readableObject = metaDataObject as? AVMetadataMachineReadableCodeObject else { return }
+            guard let stringValue = readableObject.stringValue else { return }
+            AudioServicesPlaySystemSound(kSystemSoundID_Vibrate)
+            readBarcode(stringValue)
+        }
     }
 }
